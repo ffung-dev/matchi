8b6a44b3a72fa3c18dc7fd371e6d9ba39aa892e4
ffung-dev <ffung.dev@gmail.com>
2025-07-23 19:42:13 -0400

    created main buttons

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 let inputs = []
+let prevInputs = []
+
 const inputElement = document.getElementById("input-el")
 const inputButton = document.getElementById("input-btn")
+const delButton = document.getElementById("del-btn")
+const tabLinkButton = document.getElementById("tablink-btn")
 const ulElement = document.getElementById("ul-el")
 
-inputButton.addEventListener("click", function() {
-    // console.log("button clicked")
-    inputs.push(inputElement.value)
-    inputElement.value = ""
-    updateList()
-})
+// array of inputs in local storage
+const storedInputs = JSON.parse(localStorage.getItem("inputs"))
+if (storedInputs)
+{
+    inputs = storedInputs
+    update(inputs)
+}
 
-function updateList()
+function update(items)
 {
     let listItems = ""
-    for (let i = 0 ; i < inputs.length ; i++)
+    for (let i = 0 ; i < items.length ; i++)
     {
         listItems += `
         <li>
-            <a target='_blank' href='${inputs[i]}'>
-                ${inputs[i]}
+            <a target='_blank' href='${items[i]}'>
+                ${items[i]}
             </a>
         </li>`
     }
     ulElement.innerHTML = listItems
 }
+
+// save bookmark
+inputButton.addEventListener("click", function() {
+    // console.log("button clicked")
+    inputs.push(inputElement.value)
+    inputElement.value = ""
+    // save 
+    localStorage.setItem("inputs", JSON.stringify(inputs))
+    update(inputs)
+})
+
+// save tab
+tabLinkButton.addEventListener("onclick", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        inputs.push(tabs[0].url)
+        localStorage.setItem("inputs", JSON.stringify(inputs))
+        update(inputs)
+    })
+})
+
+// delete all
+delButton.addEventListener("dblclick", function() {
+    localStorage.clear()
+    inputs = []
+    update(inputs)
+})
+
